069eaa87cc5c8526113f717f8d3939545d153441
Frank Hossfeld <frank.hossfeld@googlemail.com>
2017-01-27 14:35:10 +0100

    Pushing missing class

---
+++ Mvp4gMultiPresenterExample/src/main/java/de/gishmo/mvp4g/example/multipresenter/shared/dto/MultiPresenterModel.java
@@ -0,0 +1,27 @@
+package de.gishmo.mvp4g.example.multipresenter.shared.dto;
+
+
+public class MultiPresenterModel {
+
+  private String id;
+  private String created;
+
+  public MultiPresenterModel() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getCreated() {
+    return created;
+  }
+
+  public void setCreated(String created) {
+    this.created = created;
+  }
+}
